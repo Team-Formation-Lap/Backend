6763fe36c39d53d08a1cfcf0b4ec0568870ecca5
suyeonnii <suyeon9288@gmail.com>
2025-02-07 03:00:19 +0900

    feat:result 엔티티 정의

--- result/models.py
+++ result/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from interview.models import Interview
 
-# Create your models here.
+class Result(models.Model):
+    interview = models.OneToOneField(Interview, on_delete=models.CASCADE)
+    overall_feedback = models.TextField(null=True, blank=True)
+    behavior_feedback = models.TextField(null=True, blank=True)
+    answer_feedback = models.TextField(null=True, blank=True)
+    video_url = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"result: {self.interview.id}"
+
