83ffe4f5d738f9b9318ae44d0c7a5ef592a97d0f
user_chan <beauriful032@gmail.com>
2025-02-16 13:18:53 +0900

    fix: 미로그인 예외처리 삭제(결과업로드API)

--- result/urls.py
+++ result/urls.py
@@ -4,5 +4,7 @@ from result import views
 appname = "result"
 
 urlpatterns = [
-    path("upload/<int:interview_id>", views.ResultVideoUploadView.as_view(), name="video-upload"),
+    path("upload/<int:interview_id>",
+         views.ResultVideoUploadView.as_view(),
+         name="video-upload"),
 ]
--- result/views.py
+++ result/views.py
@@ -7,33 +7,29 @@ from django.core.files.base import ContentFile
 from result.models import Result
 from result.serializers import ResultVideoUploadSerializer
 from drf_yasg.utils import swagger_auto_schema
-from result.permission import IsAuthenticatedCustom
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
 from interview.models import Interview
 
-@method_decorator(csrf_exempt, name="dispatch")
 class ResultVideoUploadView(APIView):
     parser_classes = (MultiPartParser, FormParser)
-    permission_classes = [IsAuthenticatedCustom]
 
     @swagger_auto_schema(
         request_body=ResultVideoUploadSerializer,
         operation_id="결과 업로드 API",
     )
     def post(self, request, interview_id):
-        user = request.user
+        #user = request.user
         video = request.FILES.get("file")
 
         if not video:
             return Response({"error": "파일을 업로드하세요."}, status=status.HTTP_400_BAD_REQUEST)
 
-        if not video.name.endswith(".webm"):
+        if not video.name.endswith((".webm", ".mp4")):
             return Response({"error": "webm 형식의 영상 파일만 업로드할 수 있습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
         interview = Interview.objects.get(id=interview_id)
+        user_id = interview.user.id
 
-        video_path = f"video/{user.id}/{video.name}"
+        video_path = f"video/{user_id}/{video.name}"
         saved_path = default_storage.save(video_path, ContentFile(video.read()))
         video_url = default_storage.url(saved_path)
 
