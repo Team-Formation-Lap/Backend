da2830c107ca9979a32fab2e27255f8ed0911105
suyeonnii <suyeon9288@gmail.com>
2025-02-17 18:31:52 +0900

    feat: gpt api 연동

--- interview/views.py
+++ interview/views.py
@@ -25,7 +25,7 @@ class StartInterviewView(APIView):
             if not questions_count or not (3<=int(questions_count)<=10):
                 return JsonResponse({"error": "질문 개수는 3~10개 사이여야 합니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-            #사용자 이력서 가져오기
+            #사용자 이력서 가져오기(우선 임의값 넣어놓음)
             resume=Resume.objects.create(user_id=user_id, file_url="test_resume.pdf")
             if not resume:
                 return JsonResponse({"error": "이력서를 찾을 수 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
@@ -35,9 +35,9 @@ class StartInterviewView(APIView):
             return JsonResponse(
                 {
                     "message": "면접이 시작되었습니다.",
+                    "user_id": user_id,
                     "interview_id":interview.id,
-                    "questions_count":questions_count,
-                    "user_id":user_id
+                    "questions_count":questions_count
                 },
                 status=status.HTTP_201_CREATED
             )
