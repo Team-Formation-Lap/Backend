1382bd8ba16d8f29f17576a0fa9753529a4b6ce0
user_chan <beauriful032@gmail.com>
2025-03-30 17:57:26 +0900

    fix: 충돌 해결

--- resume/urls.py
+++ resume/urls.py
@@ -5,5 +5,6 @@ appname = "resume"
 
 urlpatterns = [
     path("upload/<user_id>", views.ResumeUploadView.as_view(), name="upload"),
+    path("list/<user_id>", views.ResumeListView.as_view(), name="resumes-list"),
     path("delete/<resume_id>", views.ResumeDeleteView.as_view(), name="delete"),
 ]
